354157a391745bd90c68672ff7c50e28af8fbe17
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-10-06 13:34:33 -0400

    added a basic navbar component to the project

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native'
+import TriviaQuestions from './TriviaQuestion'
+
+const Navbar = () => {
+    return (
+        <View>
+            <Text>
+                Navbar goes here
+            </Text>
+        </View>
+    )
+}
+
+export default Navbar
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
+import Navbar from '../components/Navbar'
 
 const HomeScreen = ({navigation}) => {
     return(
         <View>
-        <Text style={{color: '#00b300'}}>Welcome to the Next Street Mobile App.  This app will assist you with getting your permit and your license.</Text>
-        <Button 
-            title="Go to Trivia Screen"
-            onPress={() => navigation.navigate('Trivia')}
-        />
+            <Navbar/>
+            <Text style={{color: '#00b300'}}>Welcome to the Next Street Mobile App.  This app will assist you with getting your permit and your license.</Text>
+            <Button 
+                title="Go to Trivia Screen"
+                onPress={() => navigation.navigate('Trivia')}
+            />
         </View>
     )
 }
