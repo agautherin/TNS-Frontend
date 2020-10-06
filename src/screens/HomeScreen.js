046626fbb0da503eb86a7f2e3e6a224b30b41b23
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-10-06 13:28:20 -0400

    homepage updated

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,7 @@ import {View, Text, Button} from 'react-native';
 const HomeScreen = ({navigation}) => {
     return(
         <View>
-        <Text>Welcome to the Next Street Mobile App.  This app will assist you with getting your permit and your license.</Text>
+        <Text style={{color: '#00b300'}}>Welcome to the Next Street Mobile App.  This app will assist you with getting your permit and your license.</Text>
         <Button 
             title="Go to Trivia Screen"
             onPress={() => navigation.navigate('Trivia')}
