4d3d42350a9b91347227b7bcb70ef8b69166e448
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-10-06 13:51:53 -0400

    working on adding tabs for navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
-import Navbar from '../components/Navbar'
+// import MyTabs from '../components/MyTabs'
 
 const HomeScreen = ({navigation}) => {
     return(
         <View>
-            <Navbar/>
+            {/* <MyTabs /> */}
             <Text style={{color: '#00b300'}}>Welcome to the Next Street Mobile App.  This app will assist you with getting your permit and your license.</Text>
             <Button 
                 title="Go to Trivia Screen"
